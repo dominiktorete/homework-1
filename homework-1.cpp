9fe6ea950414f8cfa2371fe51046a7c400cd1c0c
dominiktorete <emec85748@gmail.com>
2025-06-03 21:06:57 +0500

    Add files via upload

---
+++ homework-1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>//Connecting a library iostream
+
+int main() {// creature a main function
+
+	setlocale (LC_ALL,"Ru-ru"); //use local ru
+
+	using std::cout; // use namespase std -> cout
+
+	cout << "Максим\n"; // output data
+
+	cout << "21" << '\n'; // output data
+
+}
