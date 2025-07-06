a571328e39a627406494855c4a70d2a1cd700028
dominiktorete <emec85748@gmail.com>
2025-07-06 11:03:23 +0500

    Update homework-10.1.cpp

--- homework-10.1.cpp
+++ homework-10.1.cpp
@@ -5,8 +5,8 @@ int main(){
     int n{};
     std::cout << "Введите размер массива: ";
     std::cin >> n;
-    int* arr = new int[n]{};
-    if(arr != nullptr){
+    int* arr = new (std::nothrow) int [n]{};
+    if(arr){
         for(int i = 0; i < n; i++){
             std::cout << "arr[" << i << "] = ";
             std::cin >> arr[i];
@@ -19,7 +19,7 @@ int main(){
         delete[] arr;
         arr = nullptr;
     }
-    else{ std::cout << "Error allocation memory!!!";
+    else{ std::cerr << "Error allocation memory!!!";
         return -1;
     }
     return 0;
