13ee787d2dc2ec2e62b0ce0d0aa552ab6f9c330b
dominiktorete <emec85748@gmail.com>
2025-07-08 19:42:37 +0500

    Add files via upload

---
+++ homework-11.1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	std::string first_name{}, last_name{}, hello{};
+	std::cout << "Введите имя: ";
+	getline(std::cin, last_name);
+	std::cout << "Введите фамилию: ";
+	getline(std::cin, first_name);
+	hello = "Здравствуйте, " + last_name + " " + first_name;
+	std::cout << hello << std::endl;
+
+
+
+	return 0;
+}
---
+++ homework-11.2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	std::string user_str{}, mystery_str = "Малина";
+	do {
+		std::cout << "Угадайте слово: ";
+		getline(std::cin, user_str);
+		if (user_str == mystery_str) {
+			std::cout << "Правильно! Вы победили! Загаданное слово — " << mystery_str << std::endl;
+			break;
+		}
+		std::cout << "Неправильно" << std::endl;
+		
+	} while (user_str != mystery_str);
+	
+
+	return 0;
+}
