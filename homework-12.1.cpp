84c2fc9b5794a03b6e33db0b1fefbebeb6aa9f97
dominiktorete <emec85748@gmail.com>
2025-07-11 20:07:01 +0500

    Add files via upload

---
+++ homework-12.1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+#include <windows.h>
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	
+	std::ifstream file("in.txt");
+	if (file.is_open()) {
+		while (!file.eof()) {
+			std::string str{};
+			file >> str;
+			std::cout << str << std::endl;
+		}
+		file.close();
+	}
+	else {
+		std::cerr << "Error: file not open" << std::endl;
+		return -1;
+	}
+
+
+	return 0;
+}
