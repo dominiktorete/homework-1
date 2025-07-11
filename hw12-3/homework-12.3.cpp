46a27b4587a0d29034df7bc212b03c06b54cc238
dominiktorete <emec85748@gmail.com>
2025-07-11 21:15:12 +0500

    Update homework-12.3.cpp

--- hw12-3/homework-12.3.cpp
+++ hw12-3/homework-12.3.cpp
@@ -1,33 +1,33 @@
-﻿//#include <iostream>
-//#include <string>
-//#include <fstream>
-//#include <windows.h>
-//int main() {
-//	SetConsoleCP(1251);
-//	SetConsoleOutputCP(1251);
-//	int n{};
-//	std::cout << "Введите размер массива: ";
-//	std::cin >> n;
-//	int* arr = new int[n] {};
-//
-//	for (int i = 0; i < n; i++) {
-//		std::cout << "arr[" << i << "] = ";
-//		std::cin >> arr[i];
-//	}
-//	std::ofstream file("in2.txt");
-//	if (file.is_open()) {
-//		file << n << std::endl;
-//		while (n > 0) {
-//			file << arr[n-1] << " ";
-//			n--;
-//		}
-//		file.close();
-//	}
-//	else {
-//		std::cerr << "Error: file not open" << std::endl;
-//		return -1;
-//	}
-//	delete[] arr;
-//
-//	return 0;
-//}
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+#include <windows.h>
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	int n{};
+	std::cout << "Введите размер массива: ";
+	std::cin >> n;
+	int* arr = new int[n] {};
+
+	for (int i = 0; i < n; i++) {
+		std::cout << "arr[" << i << "] = ";
+		std::cin >> arr[i];
+	}
+	std::ofstream file("in2.txt");
+	if (file.is_open()) {
+		file << n << std::endl;
+		while (n > 0) {
+			file << arr[n-1] << " ";
+			n--;
+		}
+		file.close();
+	}
+	else {
+		std::cerr << "Error: file not open" << std::endl;
+		return -1;
+	}
+	delete[] arr;
+
+	return 0;
+}
