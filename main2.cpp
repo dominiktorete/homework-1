60d453496e775798111aaf44ff66a5653238ae0c
dominiktorete <emec85748@gmail.com>
2025-07-19 09:33:45 +0500

    Add files via upload

---
+++ main2.cpp
@@ -0,0 +1,119 @@
+﻿//#include <iostream>
+//#include <initializer_list>
+//template <typename T>
+//class DynamicMassive {
+//    T* ptr = nullptr;
+//    size_t count = 0;
+//    size_t capacity = 0;
+//public:
+//    DynamicMassive(){}
+//    DynamicMassive(size_t _count ) : count(_count), capacity(_count){
+//        ptr = new(T[capacity]);
+//    }
+//    DynamicMassive(std::initializer_list <T> list) : count(list.size()), capacity(list.size()) {
+//        ptr = new(T[capacity]);
+//        size_t i = 0;
+//        for (auto& v : list) {
+//            *(ptr + i) = v;
+//            i++;
+//        }
+//    }
+//    ~DynamicMassive() {
+//        delete[] ptr;
+//        std::cout << "Delete massive" << std::endl;
+//        ptr = nullptr;
+//    }
+//    T* begin() {
+//        T* ptr2 = ptr;
+//        return ptr2;
+//    }
+//    T* end() {
+//        T* ptr2 = (ptr + capacity);
+//        return ptr2;
+//    }
+//    void resize(size_t new_sz) {
+//        if (new_sz <= capacity)return;
+//        capacity = new_sz;
+//        T* pt_res = new(T[capacity]);
+//        for (size_t i = 0; i <= count; i++) {
+//            pt_res[i] = ptr[i];
+//        }
+//            delete[] ptr;
+//            ptr = pt_res;
+//        
+//    }
+//    void push_back(T k) {
+//        if (count < capacity) {
+//            ptr[count] = k;
+//            count++;
+//        }
+//        else if (count >= capacity) {
+//            resize(capacity * 2);
+//            ptr[count] = k;
+//            count++;
+//        }
+//    }
+//    void pop_back() {
+//        if (count > 0) {
+//            count--;
+//        }
+//        else std::cout << "Massive is empty!" << std::endl;
+//    }
+//    int capacitys() {
+//        return capacity;
+//    }
+//    int size() {
+//        return count;
+//    }
+//    class Item {
+//        DynamicMassive* current{ nullptr };
+//        int index{ -1 };
+//
+//    public:
+//        Item(DynamicMassive* obj, int idx) : current(obj), index(idx)
+//        {
+//        }
+//
+//        operator T() const {
+//            if (index >= current->capacity || index < 0)
+//                return -1;
+//
+//            return current->ptr[index];
+//        }
+//        T operator=(T right) const {
+//            if (index >= current->capacity || index < 0)
+//                return right; // размер массива data не может превышать max_length_array элементов
+//
+//            if (index >= current->capacity) {
+//                current->resize(index + 1);
+//            }
+//
+//            for (size_t i = current->count; i < index; ++i)
+//                current->ptr[i] = 0;   // зануляем все новые добавленные значения
+//
+//            if (index >= current->count)
+//                current->count = index + 1; // новый размер записанных данных
+//
+//            current->ptr[index] = right;
+//            return right;
+//        }
+//    };
+//    Item operator[](int index)
+//    {
+//        return Item(this, index);
+//    }
+//    
+//    
+//     
+//};
+//
+//int main()
+//{
+//    DynamicMassive<double> arr{ '1','2', '3'};
+//    arr.push_back('2');
+//    arr.push_back('4');
+//    arr.pop_back();
+//    auto it = arr.begin();
+//    std::cout << arr.capacitys() << arr.size();
+//    return 0;
+//}
---
+++ trening.cpp
@@ -0,0 +1,139 @@
+﻿//#include <iostream>
+//#include <cmath> 
+//#include <utility>
+//#include <string>
+//#include <memory>
+//#include <vector>
+//#include <algorithm>
+//#include <set>
+//#include <map>
+//#include <forward_list>
+//#include <list>
+//#include <sstream>
+////void sort(int (&arr)[10]) {
+////	for (int i = 1; i < 10; i++) {
+////		for (int j = i - 1; j >= 0; j--) {
+////			if (arr[j] > arr[j + 1]) {
+////				std::swap(arr[j], arr[j + 1]);
+////			}
+////			else break;
+////		}
+////	}
+////}
+////void sort_2(int(&arr)[10]) {
+////
+////}
+////template<typename Container>
+////void pt(Container data) {
+////	for (auto& r : data) {
+////		std::cout << r << " ";
+////	}
+////}
+////void funk(std::vector<std::string>& names, std::vector<std::string>& str){
+////	 
+////}
+//int main() {
+//	
+//	using std::cin;
+//	using std::cout;
+//	using std::endl;
+//
+//		std::map<char, int> ch;
+//		std::string str;
+//		int count = 0;
+//		while (true) {
+//			getline(std::cin, str);
+//			if (str.empty())break;
+//			count++;
+//			std::set<char> w(str.begin(), str.end());
+//			for (char c : w) {
+//				ch[c]++;
+//			}
+//		}
+//		for (const auto& [c, i] : ch) {
+//			if (i == count) {
+//				std::cout << c;
+//			}
+//		}
+//	
+//	/*std::vector<int> vec1{ 1,3,4,56 };
+//	vec1.resize(vec1.size()*2);
+//	std::vector<std::string> vec;
+//	std::string buff{};
+//	std::string str{};
+//	std::list<std::string> lst;
+//	while (true) {
+//		getline(cin, str);
+//		if (str.empty()) {
+//			break;
+//		}
+//		lst.push_back(str);
+//	}
+//	auto it = lst.begin();
+//	while (getline(cin, str)) {
+//		if (str == "Ctrl+X") {
+//			buff = std::move(*it);
+//			it = lst.erase(it);
+//		}
+//		else if (str == "Ctrl+V") {
+//			lst.insert(it, buff);
+//		}
+//		else if (str == "Down") {
+//			if (it == (lst.end()))continue;
+//			else ++it;
+//		}
+//		else if (str == "Up") {
+//			if (it == lst.begin())continue;
+//			else --it;
+//		}
+//		if (str.empty()) {
+//			break;
+//		}
+//	}
+//	for (std::list<std::string>::iterator i = lst.begin(); i != lst.end(); ++i) {
+//		cout << *i << std::endl;
+//	}*/
+//	/*std::vector<std::string> name;
+//	int N{};
+//	int M{};
+//	cin >> N;
+//	for (int i = 0; i < N; i++) {
+//		std::string string1{};
+//		std::string string2{};
+//		cin >> string1 >> string2;
+//		if (string2 == "top")
+//			name.insert(name.begin(), string1);
+//		else
+//			name.push_back(string1);
+//	}
+//	cin >> M;
+//	std::vector<std::string> vec;
+//	for (int i = 0; i < M; i++) {
+//		int j{};
+//		cin >> j;
+//		vec.push_back(name[j - 1]);
+//	}
+//	for (size_t i = 0; i < vec.size(); i++) {
+//		cout << vec[i] << endl;
+//	}*/
+//	/*int x = 14;
+//	void* p = &x;
+//	cout << p << endl;
+//	int* d = reinterpret_cast <int*>(p);
+//	cout << d << endl;
+//	cout << *d << endl;*/
+//	/*int arr[10]{ 5, 12, 2, 3, 1 ,7, 8, 6, 9 , 22 };
+//	sort(arr);
+//	for (int i = 0; i < 10; i++) {
+//		cout << arr[i] << " ";
+//	}*/
+//	/*std::initializer_list list{ 1,2,3,4,5,6,7 };
+//	std::weak_ptr<std::vector<int>> sp = std::make_shared<std::vector<int>>( list );
+//	std::shared_ptr<std::vector<int>> sp2 = sp.lock();
+//	std::vector<int>* vecs = sp2.get();
+//	vecs->push_back(12);*/
+//
+//
+//
+//	return EXIT_SUCCESS;
+//}
